219012a416404fffc025517a026aa41ecee2bb7b
Pandafter <juannicolasleyvahoyos@gmail.com>
2023-07-03 16:45:19 -0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+import { logo } from "../assets"
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28  object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://juanleyva.tech/", "_blank")
+          }
+          className='black_btn font-bold'
+        >
+          Portfolio
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
